953ae4b3bea49aa65cf5342b0946a6b0db7a9ad7
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-10-02 19:32:26 +0900

    stateでボタンを押したとき画像が変わる

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ export const App = () => {
       </header>
       <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} />
+      <br />
+      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>更新</button>
     </div>
   )
 }
