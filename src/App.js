7865ac25bcda519cd90262f6de1250ec98ba20c6
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-10-05 17:08:41 +0900

    面談で色々教えていただいたコード

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
 
-  const onImgChange = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(
-        function(res) {
-          console.log(res)
-          const data = res.json()
-          console.log(data)
-          return data
-        }
-      )
-      .then(
-        function (result) {
-          console.log(result)
-          setDogUrl(result["message"])
-        }
-      )
+  const onImgChange = async () => {
+    // fetch('https://dog.ceo/api/breeds/image/random')
+    //   .then(res => {
+    //     console.log(res)
+    //     const data = res.json()
+    //     console.log(data)
+    //     return data
+    //   })
+    //   .then(result => {
+    //     console.log(result)
+    //     setDogUrl(result['message'])
+    //   })
+
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    setDogUrl(data['message'])
+
+    // 大体同義
+    // p.then(value => {
+    //   // value を使った処理
+    // })
+    //
+    // const value = await p
+    // // valueを使った処理
   }
 
   return (
@@ -38,7 +48,9 @@ export const App = () => {
         <div className="content">
           <img className="img_size" src={dogUrl} />
           <br />
-          <button className="btn" onClick={onImgChange}>更新</button>
+          <button className="btn" onClick={onImgChange}>
+            更新
+          </button>
         </div>
       </main>
     </div>
