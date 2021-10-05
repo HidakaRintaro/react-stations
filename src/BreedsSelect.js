ffdeda4e9790c19efec088908c762cbd30830c39
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-10-06 01:18:37 +0900

    section10 fin

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,24 @@
+/* eslint-disable react/prop-types */
 // DO NOT DELETE
+import React from 'react'
+
+export const BreedsSelect = props => {
+  const { breeds, selectedBreed, useSelectedBreed } = props
+
+  const onChangeBreedSelect = e => {
+    useSelectedBreed(e.target.value)
+  }
+
+  return (
+    <div className="breeds_select">
+      <label htmlFor="breeds">Breeds List</label>
+      <select id="breeds" value={selectedBreed} onChange={onChangeBreedSelect}>
+        {breeds.map((breed, index) => (
+          <option key={index} value={breed}>
+            {breed}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,9 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 // DO NOT DELETE
 import React, { useEffect, useState } from 'react'
+import { BreedsSelect } from './BreedsSelect'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([])
+  const [selectedBreed, useSelectedBreed] = useState('')
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
@@ -15,5 +17,11 @@ export const DogListContainer = () => {
       })
   }, [])
 
-  return <></>
+  return (
+    <BreedsSelect
+      breeds={breeds}
+      selectedBreed={selectedBreed}
+      useSelectedBreed={useSelectedBreed}
+    />
+  )
 }
